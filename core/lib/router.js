06df9f5787751a70f52ba3c9f3557b3c6b0ad4d4
robin.zhu <1987357952@qq.com>
2017-12-12 10:03:26 +0800

    Update router.js

--- core/lib/router.js
+++ core/lib/router.js
@@ -1,3 +1,10 @@
+/*
+* @Author: ihubcode <1987357952@qq.com>
+* @Date:   2017-12-12 10:02:23
+* @Last Modified by:   ihubcode <1987357952@qq.com>
+* @Last Modified time: 2017-12-12 10:02:23
+*/
+
 const KoaRouter = require('koa-router');
 const Router = new KoaRouter();
 const HomeController = require('../../app/controller/home.js');
