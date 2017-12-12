b25ee274a053f3b403d8de61a04f57649d6fc497
robin.zhu <1987357952@qq.com>
2017-12-12 10:21:03 +0800

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,6 @@
 * @Last Modified time: 2017-12-12 10:02:23
 */
 const Lai = require('./core/lai');
-const config = require('./config/index);
+const config = require('./config/index');
 const app = new Lai(config);
 app.run();
