e1d793905c45d1505af9a2ba1ab757ed7cad4494
robin.zhu <1987357952@qq.com>
2017-12-12 10:20:07 +0800

    Create index.js

---
+++ config/index.js
@@ -0,0 +1,22 @@
+/*
+* @Author: ihubcode <1987357952@qq.com>
+* @Date:   2017-12-12 10:02:23
+* @Last Modified by:   ihubcode <1987357952@qq.com>
+* @Last Modified time: 2017-12-12 10:02:23
+*/
+module.exports = {
+    // 端口
+    port: 7788,
+
+    // 模板引擎
+    view: {
+      type: 'nunjucks',
+      options: {}
+    },
+
+    // api接口
+    api: {
+        cnnode: '',
+        huaren: ''
+    }
+}
