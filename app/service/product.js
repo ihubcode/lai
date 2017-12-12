a9842e7b28dae593c5b2e92a3e65a9e8bb9e14c3
robin.zhu <1987357952@qq.com>
2017-12-12 10:09:14 +0800

    Create product.js

---
+++ app/service/product.js
@@ -0,0 +1,12 @@
+/*
+* @Author: ihubcode <1987357952@qq.com>
+* @Date:   2017-12-12 10:02:23
+* @Last Modified by:   ihubcode <1987357952@qq.com>
+* @Last Modified time: 2017-12-12 10:02:23
+*/
+module.exports = class Products extends baseContext {
+    constructor(ctx) {
+        super();
+        this.ctx = ctx;
+    }
+}
